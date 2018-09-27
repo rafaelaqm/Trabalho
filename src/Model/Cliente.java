06b2af84c76c98ddc464eef98472296e370f1737
wyss2 <wyss2@DESKTOP-13PBP9G>
2018-09-27 18:34:27 -0300

    Subindo o projeto criado pelo Samuel, via NetBeans

---
+++ src/Model/Cliente.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author User
+ */
+public class Cliente {
+    
+    private int codigoCliente;
+    private String nomeCliente;
+    private String rgCliente;
+    private String cpfCliente;
+    private String sexoCliente;
+    private String telefoneCliente;
+    private String emailCliente;
+    private String ruaCliente;
+    private String numeroCliente;
+    private String complementoCliente;
+    private String bairroCliente;
+    private String cidadeCliente;
+    private String estadoCliente;
+    private String cepCliente;
+
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getRgCliente() {
+        return rgCliente;
+    }
+
+    public void setRgCliente(String rgCliente) {
+        this.rgCliente = rgCliente;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public String getSexoCliente() {
+        return sexoCliente;
+    }
+
+    public void setSexoCliente(String sexoCliente) {
+        this.sexoCliente = sexoCliente;
+    }
+
+    public String getTelefoneCliente() {
+        return telefoneCliente;
+    }
+
+    public void setTelefoneCliente(String telefoneCliente) {
+        this.telefoneCliente = telefoneCliente;
+    }
+
+    public String getEmailCliente() {
+        return emailCliente;
+    }
+
+    public void setEmailCliente(String emailCliente) {
+        this.emailCliente = emailCliente;
+    }
+
+    public String getRuaCliente() {
+        return ruaCliente;
+    }
+
+    public void setRuaCliente(String ruaCliente) {
+        this.ruaCliente = ruaCliente;
+    }
+
+    public String getNumeroCliente() {
+        return numeroCliente;
+    }
+
+    public void setNumeroCliente(String numeroCliente) {
+        this.numeroCliente = numeroCliente;
+    }
+
+    public String getComplementoCliente() {
+        return complementoCliente;
+    }
+
+    public void setComplementoCliente(String complementoCliente) {
+        this.complementoCliente = complementoCliente;
+    }
+
+    public String getBairroCliente() {
+        return bairroCliente;
+    }
+
+    public void setBairroCliente(String bairroCliente) {
+        this.bairroCliente = bairroCliente;
+    }
+
+    public String getCidadeCliente() {
+        return cidadeCliente;
+    }
+
+    public void setCidadeCliente(String cidadeCliente) {
+        this.cidadeCliente = cidadeCliente;
+    }
+
+    public String getEstadoCliente() {
+        return estadoCliente;
+    }
+
+    public void setEstadoCliente(String estadoCliente) {
+        this.estadoCliente = estadoCliente;
+    }
+
+    public String getCepCliente() {
+        return cepCliente;
+    }
+
+    public void setCepCliente(String cepCliente) {
+        this.cepCliente = cepCliente;
+    }
+
+    
+}
---
+++ src/Model/Funcionario.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author User
+ */
+public class Funcionario {
+    
+    private int codigoFuncionario;
+    private String funcaoFuncionario;
+    private String salarioFuncionario;
+    private String nomeFuncionario;
+    private String rgFuncionario;
+    private String cpfFuncionario;
+    private String sexoFuncionario;
+    private String telefoneFuncionario;
+    private String emailFuncionario;
+    private String ruaFuncionario;
+    private String numeroFuncionario;
+    private String complementoFuncionario;
+    private String bairroFuncionario;
+    private String cidadeFuncionario;
+    private String estadoFuncionario;
+    private String cepFuncionario;
+
+    public int getCodigoFuncionario() {
+        return codigoFuncionario;
+    }
+
+    public void setCodigoFuncionario(int codigoFuncionario) {
+        this.codigoFuncionario = codigoFuncionario;
+    }
+
+    public String getFuncaoFuncionario() {
+        return funcaoFuncionario;
+    }
+
+    public void setFuncaoFuncionario(String funcaoFuncionario) {
+        this.funcaoFuncionario = funcaoFuncionario;
+    }
+
+    public String getSalarioFuncionario() {
+        return salarioFuncionario;
+    }
+
+    public void setSalarioFuncionario(String salarioFuncionario) {
+        this.salarioFuncionario = salarioFuncionario;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getRgFuncionario() {
+        return rgFuncionario;
+    }
+
+    public void setRgFuncionario(String rgFuncionario) {
+        this.rgFuncionario = rgFuncionario;
+    }
+
+    public String getCpfFuncionario() {
+        return cpfFuncionario;
+    }
+
+    public void setCpfFuncionario(String cpfFuncionario) {
+        this.cpfFuncionario = cpfFuncionario;
+    }
+
+    public String getSexoFuncionario() {
+        return sexoFuncionario;
+    }
+
+    public void setSexoFuncionario(String sexoFuncionario) {
+        this.sexoFuncionario = sexoFuncionario;
+    }
+
+    public String getTelefoneFuncionario() {
+        return telefoneFuncionario;
+    }
+
+    public void setTelefoneFuncionario(String telefoneFuncionario) {
+        this.telefoneFuncionario = telefoneFuncionario;
+    }
+
+    public String getEmailFuncionario() {
+        return emailFuncionario;
+    }
+
+    public void setEmailFuncionario(String emailFuncionario) {
+        this.emailFuncionario = emailFuncionario;
+    }
+
+    public String getRuaFuncionario() {
+        return ruaFuncionario;
+    }
+
+    public void setRuaFuncionario(String ruaFuncionario) {
+        this.ruaFuncionario = ruaFuncionario;
+    }
+
+    public String getNumeroFuncionario() {
+        return numeroFuncionario;
+    }
+
+    public void setNumeroFuncionario(String numeroFuncionario) {
+        this.numeroFuncionario = numeroFuncionario;
+    }
+
+    public String getComplementoFuncionario() {
+        return complementoFuncionario;
+    }
+
+    public void setComplementoFuncionario(String complementoFuncionario) {
+        this.complementoFuncionario = complementoFuncionario;
+    }
+
+    public String getBairroFuncionario() {
+        return bairroFuncionario;
+    }
+
+    public void setBairroFuncionario(String bairroFuncionario) {
+        this.bairroFuncionario = bairroFuncionario;
+    }
+
+    public String getCidadeFuncionario() {
+        return cidadeFuncionario;
+    }
+
+    public void setCidadeFuncionario(String cidadeFuncionario) {
+        this.cidadeFuncionario = cidadeFuncionario;
+    }
+
+    public String getEstadoFuncionario() {
+        return estadoFuncionario;
+    }
+
+    public void setEstadoFuncionario(String estadoFuncionario) {
+        this.estadoFuncionario = estadoFuncionario;
+    }
+
+    public String getCepFuncionario() {
+        return cepFuncionario;
+    }
+
+    public void setCepFuncionario(String cepFuncionario) {
+        this.cepFuncionario = cepFuncionario;
+    }
+    
+    
+    
+}
